0e11c3a356ce3ae215b49b0d809c193f9b60154b
Andre Tabourian <andretabourian@hotmail.com>
2022-04-26 05:28:53 +0200

    Added script to automatically download packer.nvim on first startup

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -1,4 +1,12 @@
 
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
 
 return require'packer'.startup(function()
     use 'wbthomason/packer.nvim' -- Plugin manager
@@ -28,5 +36,11 @@ return require'packer'.startup(function()
         'hrsh7th/vim-vsnip',
         'onsails/lspkind-nvim'
     }
+
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+
 end)
 
